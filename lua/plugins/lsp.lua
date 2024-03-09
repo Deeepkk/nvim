f55e65147827eee0abf71ac873a29ff7e15f0af8
Deepak <duxedocontact@gmail.com>
2024-03-09 15:59:04 +0530

    unable to format on save

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,7 +36,7 @@ return {
 		config = function()
 			vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})
 			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', '<space>f', function()
+			vim.keymap.set('n', '<leader>f', function()
 				vim.lsp.buf.format { async = true }
 			end, {})
 		end
