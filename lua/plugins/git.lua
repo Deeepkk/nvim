95f346771ff2b4cda2c0a0f2d4d72766aebbced6
Deepak <duxedocontact@gmail.com>
2024-03-09 16:37:45 +0530

    gitsigns not working? colorscheme ?

---
+++ lua/plugins/git.lua
@@ -0,0 +1,19 @@
+return {
+
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function ()
+			require('gitsigns').setup()
+		end
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>')
+			vim.keymap.set('n', '<leader>ga', '<cmd>Git add -A<CR>')
+			vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>')
+			vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
+			vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<CR>')
+		end
+	}
+}
