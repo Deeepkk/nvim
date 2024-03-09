d72b48b3c7e4fecdee47a684ca4b8a4314adc46b
Deepak <duxedocontact@gmail.com>
2024-03-09 14:58:46 +0530

    fugitive pull

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,5 +5,6 @@ return {
 	vim.keymap.set('n', '<leader>ga', '<cmd>Git add -A<CR>')
 	vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>')
 	vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
+	vim.keymap.set('n', '<leader>gl', '<cmd>Git pull<CR>')
 	end
 }
