e95959785a3934868ccca226f266a8e73d731aeb
Deepak <duxedocontact@gmail.com>
2024-03-09 14:19:52 +0530

    fugitive

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,9 @@
+return {
+	"tpope/vim-fugitive",
+	config = function ()
+	vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>')
+	vim.keymap.set('n', '<leader>ga', '<cmd>Git add -A<CR>')
+	vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>')
+	vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
+	end
+}
