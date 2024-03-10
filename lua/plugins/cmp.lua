eb886b1a1be847ca2ad125ae9499e96e45e7f025
Deepak <duxedocontact@gmail.com>
2024-03-10 11:40:47 +0530

    cmp *

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,20 +1,59 @@
 return {
+	-- cmp
 	{
-		'neovim/nvim-lspconfig',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/nvim-cmp',
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
-		"rafamadriz/friendly-snippets",
+		-- Snippets
+		{
+			"L3MOn4D3/Luasnip",
+			dependencies = {
+				'saadparwaiz1/cmp_luasnip',
+				"rafamadriz/friendly-snippets",
+			},
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		-- cmp setup
+		{
+			'hrsh7th/nvim-cmp',
+			dependencies = {
+				'neovim/nvim-lspconfig',
+				'hrsh7th/cmp-nvim-lsp',
+				'hrsh7th/cmp-buffer',
+				'hrsh7th/cmp-path',
+			},
+			config = function()
+				local cmp = require("cmp")
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							require('luasnip').lsp_expand(args.body)
+						end,
+					},
+					window = {
+						completion = cmp.config.window.bordered(),
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<tab>"] = cmp.mapping.select_next_item({
+							behavior = cmp.SelectBehavior
+							    .Insert
+						}),
+						["<S-tab>"] = cmp.mapping.select_prev_item({
+							behavior = cmp.SelectBehavior
+							    .Insert
+						}),
+						["<CR>"] = cmp.mapping.confirm({ select = true }),
+					}),
+					sources = {
+						{ name = "nvim_lsp" },
+						{ name = "luasnip" },
+						{ name = "buffer" },
+						{ name = "path" },
+					},
+				})
+			end,
+		},
 	},
-	{
-		"L3MOn4D3/Luasnip",
-		config = function()
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
-	},
+	-- copilot
 	{
 		"github/copilot.vim",
 		config = function()
@@ -25,48 +64,20 @@ return {
 		end,
 
 	},
+	-- autopairs and autotag
 	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-autopairs").setup()
-		end,
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			config = function()
+				require("nvim-autopairs").setup()
+			end,
+		},
+		{
+			"windwp/nvim-ts-autotag",
+			config = function()
+				require("nvim-ts-autotag").setup()
+			end,
+		},
 	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		'hrsh7th/nvim-cmp',
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-					["<S-tab>"] = cmp.mapping.select_prev_item({
-						behavior = cmp.SelectBehavior
-						    .Insert
-					}),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "buffer" },
-					{ name = "path" },
-				},
-			})
-		end,
-	},
-
 }
