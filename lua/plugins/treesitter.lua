6f592ca978291857cd043270fdf3072ebb6b266f
Deepak <duxedocontact@gmail.com>
2024-03-20 17:24:08 +0530

    idk

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,6 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "javascript", "html", "go" },
 			sync_install = false,
 			auto_install = true,
 			highlight = { enable = true },
