d10452ae1895ac6009e0ff8606fcebc68c726285
Deepak <duxedocontact@gmail.com>
2024-03-27 21:26:25 +0530

    idk

--- lua/plugins/css.lua
+++ lua/plugins/css.lua
@@ -15,7 +15,7 @@ return {
 		"roobert/tailwindcss-colorizer-cmp.nvim",
 		config = function()
 			require("tailwindcss-colorizer-cmp").setup({
-				color_square_width = 6,
+				color_square_width = 2,
 			})
 			require("cmp").setup = {
 				formatting = {
